3d838ac6a5be74e5357cf8516f7b42aee9524572
Victor Lopes <99993119+Victor-Lopes-Belo@users.noreply.github.com>
2025-08-21 21:18:38 -0300

    Add files via upload
    commit inicial

---
+++ apoio/src/main/java/com/plataformaapoio/ApoioApplication.java
@@ -0,0 +1,13 @@
+package com.plataformaapoio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoioApplication.class, args);
+	}
+
+}
---
+++ apoio/src/test/java/com/plataformaapoio/ApoioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataformaapoio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
