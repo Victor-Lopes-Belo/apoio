857e11538771a1897b61e670a9933eb1f4ac7e76
Victor Lopes <victorlopesbelo2004@gmail.com>
2025-11-27 18:33:39 -0300

    Rename package from com.plataformadeeventos to com.plataformaapoio
    Refactored the ApoioApplication class and its package structure to use 'com.plataformaapoio' instead of 'com.plataformadeeventos'. This improves consistency with the new naming convention.

--- apoio/src/main/java/com/plataformaapoio/ApoioApplication.java
+++ apoio/src/main/java/com/plataformaapoio/ApoioApplication.java
@@ -1,4 +1,4 @@
-package com.plataformadeeventos;
+package com.plataformaapoio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
