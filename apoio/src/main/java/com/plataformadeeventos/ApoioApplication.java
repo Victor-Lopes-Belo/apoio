66652931e57cee4716b3da62754c5f2a7410459f
Victor Lopes <victorlopesbelo2004@gmail.com>
2025-11-27 18:23:44 -0300

    ajuste master
    ajuste master

--- apoio/src/main/java/com/plataformadeeventos/ApoioApplication.java
+++ apoio/src/main/java/com/plataformadeeventos/ApoioApplication.java
@@ -1,4 +1,4 @@
-package com.plataformaapoio;
+package com.plataformadeeventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
